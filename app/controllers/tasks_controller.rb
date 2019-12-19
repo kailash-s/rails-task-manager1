af7741a7e15a13ad66902610bffee851d8c3d20f
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2019-12-19 19:21:42 +0000

    added index and show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,20 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
